61e9f748823148172dcd9ddeaf3214d96aeb28ea
Vishihereforcodes <sg.vishalkumar17@gmail.com>
2021-04-18 13:29:03 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css';
+import Home from './pages/Home';
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+      
+      <Switch>
+      <Route path="/search">
+        <SearchPage/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+      </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyA-1291NHcG1XITo7nYmlwdoP2320-nWLQ";
+
+export default API_KEY;
